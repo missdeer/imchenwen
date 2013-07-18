75c83ea85a4b3e7dfaaf2b19cdf7a4bc43449a25
missdeer <missdeer@gmail.com>
2013-07-18 17:57:05 +0800

    (*)add view headers

--- src/historymodel.cpp
+++ src/historymodel.cpp
@@ -7,25 +7,42 @@ HistoryModel::HistoryModel(QObject *parent) :
 
 QModelIndex HistoryModel::index(int row, int column, const QModelIndex &parent) const
 {
-
+    return QModelIndex();
 }
 
 QModelIndex HistoryModel::parent(const QModelIndex &index) const
 {
-
+    return QModelIndex();
 }
 
 int HistoryModel::columnCount(const QModelIndex &parent) const
 {
-
+    return 3; // title, original url, target url
 }
 
 int HistoryModel::rowCount(const QModelIndex &parent) const
 {
-
+    return 0;
 }
 
 QVariant HistoryModel::data(const QModelIndex &index, int role) const
 {
+    return QVariant();
+}
 
+QVariant HistoryModel::headerData(int section, Qt::Orientation orientation, int role) const
+{
+    if ( role == Qt::DisplayRole)
+    switch(section)
+    {
+    case 0:
+        return QVariant(tr("Title"));
+    case 1:
+        return QVariant(tr("Original URL"));
+    case 2:
+        return QVariant(tr("Target URL"));
+    default:
+        break;
+    }
+    return QVariant();
 }
--- src/resourcemodel.h
+++ src/resourcemodel.h
@@ -14,6 +14,7 @@ public:
     int columnCount(const QModelIndex & parent = QModelIndex()) const;
     int rowCount(const QModelIndex & parent = QModelIndex()) const ;
     QVariant data(const QModelIndex & index, int role = Qt::DisplayRole) const;
+    QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const;
 signals:
     
 public slots:
