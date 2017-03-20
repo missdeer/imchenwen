2d378b50744a621d87df13952261647623bd62df
Fan Yang <me@minidump.info>
2017-03-20 12:15:01 +0800

    (*)parse from ykdl/you-get output json directly

--- src/parsed/ykdl.go
+++ src/parsed/ykdl.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"bufio"
 	"encoding/json"
 	"io/ioutil"
 	"log"
 	"os/exec"
-	"strings"
+	"strconv"
 )
 
 func parseByYKDLJSON(u string, r chan interface{}) {
@@ -51,7 +50,7 @@ startProcess:
 func parseByYKDL(u string, r chan *CmdResponse) {
 	tryCount := 1
 startProcess:
-	cmd := exec.Command("ykdl.py", "-i", u)
+	cmd := exec.Command("ykdl", "-i", "--json", u)
 	stdout, err := cmd.StdoutPipe()
 	if err != nil {
 		log.Println("ykdl stdout pipe failed", err)
@@ -65,105 +64,7 @@ startProcess:
 		return
 	}
 
-	resp := &CmdResponse{}
-	var stream *Stream
-	scanner := bufio.NewScanner(stdout)
-	scanner.Split(bufio.ScanLines)
-	status := 0
-	for scanner.Scan() {
-		line := scanner.Text()
-		switch status {
-		case 0:
-			if strings.HasPrefix(line, "site:") {
-				pos := strings.IndexByte(line, byte(' '))
-				for line[pos] == ' ' {
-					pos++
-				}
-				resp.Site = line[pos:]
-				status = 1
-			}
-		case 1:
-			if strings.HasPrefix(line, "title:") {
-				pos := strings.IndexByte(line, byte(' '))
-				for line[pos] == ' ' {
-					pos++
-				}
-				resp.Title = line[pos:]
-				status = 2
-			}
-		case 2:
-			if strings.HasPrefix(line, "artist:") {
-				pos := strings.IndexByte(line, byte(' '))
-				for line[pos] == ' ' {
-					pos++
-				}
-				resp.Artist = line[pos:]
-				status = 3
-			}
-		case 3:
-			if strings.HasPrefix(line, "streams:") {
-				status = 4
-			}
-		case 4:
-			if strings.Contains(line, "- format:") {
-				pos := strings.IndexByte(line, byte(':')) + 1
-				for line[pos] == ' ' {
-					pos++
-				}
-				stream = &Stream{
-					Format: line[pos:],
-				}
-				resp.Streams = append(resp.Streams, stream)
-				status = 5
-			}
-		case 5:
-			if strings.Contains(line, "container:") {
-				pos := strings.IndexByte(line, byte(':')) + 1
-				for line[pos] == ' ' {
-					pos++
-				}
-				stream.Container = line[pos:]
-				status = 6
-			}
-		case 6:
-			if strings.Contains(line, "video-profile:") {
-				pos := strings.IndexByte(line, byte(':')) + 1
-				for line[pos] == ' ' {
-					pos++
-				}
-				stream.VideoProfile = line[pos:]
-				status = 7
-			}
-		case 7:
-			if strings.Contains(line, "# download-with:") {
-				pos := strings.IndexByte(line, byte(':')) + 1
-				for line[pos] == ' ' {
-					pos++
-				}
-				stream.DownloadWith = line[pos:]
-				status = 8
-			}
-		case 8:
-			if strings.HasPrefix(line, "Real urls:") {
-				stream.RealURLs = []string{}
-				status = 9
-			}
-		case 9:
-			if strings.Contains(line, "- format:") {
-				pos := strings.IndexByte(line, byte(':')) + 1
-				for line[pos] == ' ' {
-					pos++
-				}
-				stream = &Stream{
-					Format: line[pos:],
-				}
-				resp.Streams = append(resp.Streams, stream)
-				status = 5
-				break
-			}
-			stream.RealURLs = append(stream.RealURLs, line)
-		}
-	}
+	content, err := ioutil.ReadAll(stdout)
 	err = cmd.Wait()
 	if err != nil {
 		if tryCount < 3 {
@@ -175,6 +76,32 @@ startProcess:
 		return
 	}
 
+	var res YouGetJSON
+	err = json.Unmarshal(content, &res)
+	if err != nil {
+		log.Println("unmarshalling ykdl output JSON failed", err)
+		r <- nil
+		return
+	}
+	resp := &CmdResponse{
+		Site:    res.Site,
+		Title:   res.Title,
+		Streams: []*Stream{},
+	}
+	for _, s := range res.Streams {
+		stream := &Stream{
+			ITag:         s.ITag,
+			Quality:      s.Quality,
+			VideoProfile: s.VideoProfile,
+			Size:         strconv.Itoa(s.Size),
+			Container:    s.Container,
+			RealURLs:     s.URLs,
+		}
+		if s.URL != "" {
+			stream.RealURLs = append(stream.RealURLs, s.URL)
+		}
+		resp.Streams = append(resp.Streams, stream)
+	}
 	r <- resp
 }
 
