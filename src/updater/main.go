60ac41a5d0b5685f84eeee6ce9e9d4d0183bf5d3
Fan Yang <missdeer@dfordsoft.com>
2018-01-05 11:12:22 +0800

    (*)search download link

--- src/updater/main.go
+++ src/updater/main.go
@@ -100,10 +100,52 @@ doRequest:
 	return
 }
 
+func searchExeDownloadURL(u, tagPattern, exePattern string) (exe string) {
+	retry := 0
+	request, err := http.NewRequest("GET", u, nil)
+	if err != nil {
+		log.Println("Could not parse getting latest release page request:", err)
+		return
+	}
+
+	request.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:45.0) Gecko/20100101 Firefox/45.0")
+	request.Header.Set("Accept", "application/json, text/javascript, */*")
+	request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+doRequest:
+	resp, err := client.Do(request)
+	if err != nil {
+		log.Println("Could not send getting latest release page request:", err)
+		retry++
+		if retry < 3 {
+			time.Sleep(3 * time.Second)
+			goto doRequest
+		}
+		return
+	}
+
+	defer resp.Body.Close()
+
+	regex := regexp.MustCompile(tagPattern)
+	scanner := bufio.NewScanner(resp.Body)
+	for scanner.Scan() {
+		line := scanner.Text()
+		ss := regex.FindAllString(line, -1)
+		if len(ss) > 0 {
+			releaseURL := "https://github.com" + ss[0]
+			exe = getExeDownloadURL(releaseURL, exePattern)
+			if exe != "" {
+				return
+			}
+		}
+	}
+	return
+}
+
 func updateYouGet() {
-	latestReleaseURL := `https://github.com/soimort/you-get/releases/latest`
-	pattern := `\/soimort\/you-get\/releases\/download\/v[0-9\.]+\/you\-get\-[0-9\.]+\-win32.exe`
-	exeURL := getExeDownloadURL(latestReleaseURL, pattern)
+	latestReleaseURL := `https://github.com/soimort/you-get/tags`
+	tagPattern := `\/soimort\/you\-get\/releases\/tag\/v[0-9\.]+`
+	exePattern := `\/soimort\/you\-get\/releases\/download\/v[0-9\.]+\/you\-get\-[0-9\.]+\-win32.exe`
+	exeURL := searchExeDownloadURL(latestReleaseURL, tagPattern, exePattern)
 	if exeURL != "" {
 		filePath := `you-get.exe`
 		if executable, err := osext.Executable(); err == nil {
