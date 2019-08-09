ed36dba3d21ba8e867784b21894bbbf6e11ae473
Fan Yang <me@minidump.info>
2019-08-09 15:31:20 +0800

    (*)clone only the last commit of ykdl

--- mac/update-resolvers.sh
+++ mac/update-resolvers.sh
@@ -2,7 +2,7 @@
 brew update 
 brew upgrade annie youtube-dl
 pip3 install --upgrade you-get
-git clone https://github.com/zhangn1985/ykdl.git
+git clone --depth=1 https://github.com/zhangn1985/ykdl.git
 cd ykdl
 python3 setup.py install
 cd ..
