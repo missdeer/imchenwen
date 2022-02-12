f67c91646b3daf09d4d6eb4a232d3f52dd0c078f
Fan Yang <missdeer@gmail.com>
2022-02-12 21:49:37 +0800

    update update resolvers script

--- mac/update-resolvers.sh
+++ mac/update-resolvers.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-brew update 
-brew upgrade annie youtube-dl
-pip3 install --upgrade git+git://github.com/zhangn1985/ykdl.git
-pip3 install --upgrade git+git://github.com/soimort/you-get.git
+pip3 install https://github.com/SeaHOH/ykdl/archive/master.zip --upgrade
+pip3 install https://github.com/ytdl-org/youtube-dl/archive/refs/heads/master.zip --upgrade
+pip3 install https://github.com/soimort/you-get/archive/refs/heads/develop.zip --upgrade
+go install github.com/iawia002/lux@latest
