ddcf5d577f4741a12b86988e9539469a7c78ac57
Fan Yang <missdeer@gmail.com>
2023-11-24 11:02:19 +0800

    update mpv binary link

--- scripts/win-install-mpv.bat
+++ scripts/win-install-mpv.bat
@@ -1,11 +1,12 @@
 : Download libmpv
-curl -Lo libmpv.7z https://udomain.dl.sourceforge.net/project/mpv-player-windows/libmpv/mpv-dev-x86_64-20230611-git-1c82d6a.7z
+curl -Lo libmpv.7z https://udomain.dl.sourceforge.net/project/mpv-player-windows/libmpv/mpv-dev-x86_64-v3-20231112-git-7cab30c.7z
 
 : Extract libmpv
-7z x -olibmpv libmpv.7z
+"C:\Program Files\7-Zip\7z.exe" x -y -olibmpv libmpv.7z
 
 : Produce .lib file
 cd libmpv
+del /q mpv.def.bak
 rename mpv.def mpv.def.bak
 echo EXPORTS > mpv.def
 type mpv.def.bak >> mpv.def
