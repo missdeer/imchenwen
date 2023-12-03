42089c88db58deeff51f95d748673e5d45eaa7e4
Fan Yang <missdeer@gmail.com>
2023-12-03 19:15:23 +0800

    add logs

--- scripts/copy_filelist.sh
+++ scripts/copy_filelist.sh
@@ -10,16 +10,17 @@ cat "$script_dir/filelist.txt" | while read file; do
 		mkdir -p "$target_dir"
 	fi
 	if [ ! -f "$PWD/$file" ]; then
+		echo "check /$1/bin/$file"
 		if [ -f "/$1/bin/$file" ]; then
 			echo "cp /$1/bin/$file $PWD/$file"
 			cp "/$1/bin/$file" "$PWD/$file"
 		fi
 	fi
 	if [ ! -f "$PWD/$file" ]; then
+		echo "check /$1/share/qt6/$file"
 		if [ -f "/$1/share/qt6/$file" ]; then
 			echo "cp /$1/share/qt6/$file $PWD/$file"
 			cp "/$1/share/qt6/$file" "$PWD/$file"
 		fi
 	fi
 done
-
