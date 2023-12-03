78602adb8c40e0b8f351e86047c2a3cbd0e497de
Fan Yang <missdeer@gmail.com>
2023-12-03 20:44:34 +0800

    remove last \r

--- scripts/copy_filelist.sh
+++ scripts/copy_filelist.sh
@@ -2,6 +2,7 @@
 script_path=$(realpath "$0")
 script_dir=$(dirname "$script_path")
 ls -l $script_dir
+sed -i 's/\r$//' "$script_dir/filelist.txt"
 cat "$script_dir/filelist.txt" | while read file; do
 	echo "check $PWD/$file"
 	target_dir=$(dirname "$PWD/$file")
