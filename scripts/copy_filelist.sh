0457ac81df884f35db8c81546c95ca059cc5af5e
Fan Yang <missdeer@gmail.com>
2023-12-03 18:59:10 +0800

    add logs

--- scripts/copy_filelist.sh
+++ scripts/copy_filelist.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 script_path=$(realpath "$0")
 script_dir=$(dirname "$script_path")
+ls -l $script_dir
 cat "$script_dir/filelist.txt" | while read file; do
-    target_dir=$(dirname "$file")
+	echo "check $PWD/$file"
+	target_dir=$(dirname "$PWD/$file")
 
-    if [ ! -d "$target_dir" ]; then
-        mkdir -p "$target_dir"
-    fi
-    if [ ! -f "$file" ]; then
-        if [ -f "/$1/bin/$file" ]; then echo "cp /$1/bin/$file $PWD/$file"; cp "/$1/bin/$file" "$PWD/$file"; fi    
-    fi
-    if [ ! -f "$file" ]; then
-        if [ -f "/$1/share/qt6/$file" ]; then echo "cp /$1/share/qt6/$file $PWD/$file"; cp "/$1/share/qt6/$file" "$PWD/$file"; fi
-    fi
+	if [ ! -d "$target_dir" ]; then
+		mkdir -p "$target_dir"
+	fi
+	if [ ! -f "$PWD/$file" ]; then
+		if [ -f "/$1/bin/$file" ]; then
+			echo "cp /$1/bin/$file $PWD/$file"
+			cp "/$1/bin/$file" "$PWD/$file"
+		fi
+	fi
+	if [ ! -f "$PWD/$file" ]; then
+		if [ -f "/$1/share/qt6/$file" ]; then
+			echo "cp /$1/share/qt6/$file $PWD/$file"
+			cp "/$1/share/qt6/$file" "$PWD/$file"
+		fi
+	fi
 done
+
