1b5b05149cb2393bdb3ac446691c6551612147ad
Fan Yang <missdeer@gmail.com>
2023-12-03 16:12:19 +0800

    copy files that windeployqt missed with MinGW

---
+++ scripts/copy_filelist.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+script_path=$(realpath "$0")
+script_dir=$(dirname "$script_path")
+cat "$script_dir/filelist.txt" | while read file; do
+if [ ! -f "$file" ]; then
+if [ -f "/$1/bin/$file" ]; then cp "/$1/bin/$file" "$file"; fi    
+fi
+if [ ! -f "$file" ]; then
+if [ -f "/$1/share/qt6/$file" ]; then cp "/$1/share/qt6/$file" "$file"; fi
+fi
+done
