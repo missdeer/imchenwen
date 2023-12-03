7192de688765048716d9c24d6d26a5d08e0c0422
Fan Yang <missdeer@gmail.com>
2023-12-03 18:42:34 +0800

    print logs

--- scripts/copy_filelist.sh
+++ scripts/copy_filelist.sh
@@ -8,9 +8,9 @@ cat "$script_dir/filelist.txt" | while read file; do
         mkdir -p "$target_dir"
     fi
     if [ ! -f "$file" ]; then
-        if [ -f "/$1/bin/$file" ]; then cp "/$1/bin/$file" "$file"; fi    
+        if [ -f "/$1/bin/$file" ]; then echo "cp /$1/bin/$file $PWD/$file"; cp "/$1/bin/$file" "$PWD/$file"; fi    
     fi
     if [ ! -f "$file" ]; then
-        if [ -f "/$1/share/qt6/$file" ]; then cp "/$1/share/qt6/$file" "$file"; fi
+        if [ -f "/$1/share/qt6/$file" ]; then echo "cp /$1/share/qt6/$file $PWD/$file"; cp "/$1/share/qt6/$file" "$PWD/$file"; fi
     fi
 done
